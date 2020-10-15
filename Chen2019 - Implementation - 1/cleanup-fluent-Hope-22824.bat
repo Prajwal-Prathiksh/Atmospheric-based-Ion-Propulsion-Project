364ac8921882ac22d2038d937f0286bc5e5e0414
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-10-16 00:51:01 +0530

    Minor changes

--- Chen2019 - Implementation - 1/cleanup-fluent-Hope-22824.bat
+++
@@ -1,9 +0,0 @@
-echo off
-set LOCALHOST=%COMPUTERNAME%
-set KILL_CMD="C:\PROGRA~1\ANSYSI~1\v202\fluent/ntbin/win64/winkill.exe"
-
-"C:\PROGRA~1\ANSYSI~1\v202\fluent\ntbin\win64\tell.exe" Hope 49505 CLEANUP_EXITING
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 18220) 
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 22824) 
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 24084)
-del "E:\IIT Bombay - Miscellaneous\ITSP - Simulation Files\Atmospheric-based-Ion-Propulsion-Project\Chen2019 - Implementation - 1\cleanup-fluent-Hope-22824.bat"
--- Chen2019 - Implementation - 1/cleanup-fluent-Hope-9440.bat
+++
@@ -1,9 +0,0 @@
-echo off
-set LOCALHOST=%COMPUTERNAME%
-set KILL_CMD="C:\PROGRA~1\ANSYSI~1\v202\fluent/ntbin/win64/winkill.exe"
-
-"C:\PROGRA~1\ANSYSI~1\v202\fluent\ntbin\win64\tell.exe" Hope 49931 CLEANUP_EXITING
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 23748) 
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 9440) 
-if /i "%LOCALHOST%"=="Hope" (%KILL_CMD% 21128)
-del "E:\IIT Bombay - Miscellaneous\ITSP - Simulation Files\Atmospheric-based-Ion-Propulsion-Project\Chen2019 - Implementation - 1\cleanup-fluent-Hope-9440.bat"

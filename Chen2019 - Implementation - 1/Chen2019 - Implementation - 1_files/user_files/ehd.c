15a4f0bb72a8d651ad6a01219cd024d7cf7fa69a
Jamun-Fanatic-Foreva <prajwal.1305@gmail.com>
2020-10-29 18:47:28 +0530

    Minor Updates

--- Chen2019 - Implementation - 1/Chen2019 - Implementation - 1_files/user_files/ehd.c
+++ Chen2019 - Implementation - 1/Chen2019 - Implementation - 1_files/user_files/ehd.c
@@ -103,7 +103,7 @@ DEFINE_SOURCE(RHO_Q_source, c, t, dS, eqn)
     // Initialise source value 
 
     gradrho_dot_gradphi = C_UDSI_G(c, t, RHO_Q)[x]*C_UDSI_G(c, t, PHI)[x] + C_UDSI_G(c, t, RHO_Q)[y]*C_UDSI_G(c, t, PHI)[y];
-    source = gradrho_dot_gradphi + C_UDSI(c, t, RHO_Q)*C_UDSI(c, t, RHO_Q)/eps_o;
+    source = mu*gradrho_dot_gradphi - C_UDSI(c, t, RHO_Q)*C_UDSI(c, t, RHO_Q)/eps_o;
 
     dS[eqn] = 0.0;
     
@@ -116,24 +116,18 @@ DEFINE_UDS_FLUX(RHO_Q_flux,f,t,i)
   cell_t  c0,  c1 = -1;
   Thread *t0, *t1 = NULL;
 
-  real NV_VEC(psi_vec), NV_VEC(A), flux = 0.0, temp1, temp2;
+  real NV_VEC(psi_vec), NV_VEC(A), flux = 0.0;
 
   c0 = F_C0(f,t);
   t0 = F_C0_THREAD(f,t);
   F_AREA(A, f, t);
-
-  temp1 = -1.0 * mu * C_UDSI_G(c0,t0,PHI)[x];
-  temp2 = -1.0 * mu * C_UDSI_G(c0,t0,PHI)[y];
 
   /* If face lies at domain boundary, use face values; */
   /* If face lies IN the domain, use average of adjacent cells. */
 
  if (BOUNDARY_FACE_THREAD_P(t)) /*Most face values will be available*/
     {
-
-      //NV_DS(psi_vec,  =, temp1+F_U(f,t), temp2+F_V(f,t), F_W(f,t), *, 1.0);
-      NV_DS(psi_vec,  =, temp1, temp2, F_W(f,t), *, 1.0);
-
+      NV_DS(psi_vec,  =, F_U(f,t), F_V(f,t), F_W(f,t), *, 1.0);
       flux = NV_DOT(psi_vec, A); /* flux through Face */
     }
   else
@@ -141,13 +135,8 @@ DEFINE_UDS_FLUX(RHO_Q_flux,f,t,i)
       c1 = F_C1(f,t);       /* Get cell on other side of face */
       t1 = F_C1_THREAD(f,t); 
 
-      //NV_DS(psi_vec,  =, temp1+C_U(c0,t0),temp2+C_V(c0,t0),C_W(c0,t0),*,1.0);
-      //NV_DS(psi_vec,  =, temp1,temp2,C_W(c0,t0),*,1.0);
-
-      temp1 = -1.0 * mu * C_UDSI_G(c1,t1,PHI)[x];
-      temp2 = -1.0 * mu * C_UDSI_G(c1,t1,PHI)[y];
-      //NV_DS(psi_vec, +=, temp1+C_U(c1,t1),temp2+C_V(c1,t1),C_W(c1,t1),*,1.0);
-      NV_DS(psi_vec, +=, temp1,temp2,C_W(c1,t1),*,1.0);
+      NV_DS(psi_vec,  =, C_U(c0,t0),C_V(c0,t0),C_W(c0,t0),*,1.0);
+      NV_DS(psi_vec, +=, C_U(c1,t1),C_V(c1,t1),C_W(c1,t1),*,1.0);
 
       flux = NV_DOT(psi_vec, A)/2.0; /* Average flux through face */
     }
